e697b007996b318bf97c1f7c2967a4999c4b3dda
fnikitin <frederic.nikitin@gmail.com>
2016-09-22 12:07:57 +0200

    Moving driver to test/

--- src/test/java/org/nextprot/CucumberRunTest.java
+++ src/test/java/org/nextprot/CucumberRunTest.java
@@ -2,11 +2,13 @@ package org.nextprot;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".", tags = "@active", dryRun = true)
 public class CucumberRunTest {
 
-
+    @Test
+    public void intellijDoesNotCreateAWarningOverTheClassAnymore() {}
 }
