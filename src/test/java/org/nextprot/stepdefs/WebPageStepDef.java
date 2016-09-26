279e2f9437a9f9fd5b4c2de833a08e99661582cf
fnikitin <frederic.nikitin@gmail.com>
2016-09-26 12:24:42 +0200

    Parametizing some step definition methods

--- src/test/java/org/nextprot/stepdefs/WebPageStepDef.java
+++ src/test/java/org/nextprot/stepdefs/WebPageStepDef.java
@@ -1,7 +1,6 @@
 package org.nextprot.stepdefs;
 
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
@@ -11,63 +10,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebPageStepDef {
 
-    public static String API    = "http://dev-api.nextprot.org/";
-    public static String SEARCH = "http://dev-search.nextprot.org/";
-    public static String SNORQL = "http://dev-snorql.nextprot.org/";
+    private static String API    = "http://dev-api.nextprot.org/";
+    private static String SEARCH = "http://dev-search.nextprot.org/";
+    private static String SNORQL = "http://dev-snorql.nextprot.org/";
 
-    WebDriver driver;
-
-    @And("^I am connected with my google account$")
-    public void iAmConnectedWithMyGoogleAccount() {
-    }
-
-    @Then("^I am logged \"([^\"]*)\"$")
-    public void iAmLogged(String logged) {
-    }
-
-    @When("^I click on 'Login' button$")
-    public void iClickOnLoginButton() {
-    }
-
-    @When("^I click on 'Logout' button$")
-    public void iClickOnLogoutButton() {
-    }
-
-    @Given("^I am on page \"([^\"]*)\"$")
-    public void iAmOnPage(String page) throws Throwable {
-
-        Assert.assertEquals(getUrl(page), driver.getCurrentUrl());
-    }
-
-    @Given("^I navigate to nextprot search page$")
-    public void iShouldNavigateToSearchNextprotSearchPage() {
-
-        driver = new FirefoxDriver();
-        driver.navigate().to(SEARCH);
-    }
-
-    @When("^I click 'Release contents' link$")
-    public void i_click_on_button_Release_contents() {
-
-        driver.findElement(By.linkText("Release contents")).click();
-    }
-
-    @Then("^I should see the Application release version$")
-    public void i_check_that_Application_release_is_defined() {
-
-        String text = driver.findElement(By.xpath("//p[@class=\"ng-binding\"]")).getText();
-        Assert.assertEquals("Data release: 2016-08-25", text);
-        driver.quit();
-    }
-
-    @Then("^page should be valid$")
-    public void pageShouldBeValid() throws Throwable {
-
-        iAmOnPage("search");
-        driver.quit();
-    }
-
-    public static String getUrl(String page) {
+    private static String getNextprotPageUrl(String page) {
 
         switch (page) {
             case "api":
@@ -81,14 +28,51 @@ public class WebPageStepDef {
         }
     }
 
-    @Given("^I am on nextprot search page$")
-    public void iAmOnNextprotSearchPage() throws Throwable {
+    private WebDriver driver;
+
+    @When("^I am on nextprot page \"([^\"]*)\"$")
+    public void shouldNavigateToNextprotPage(String pageName) throws Throwable {
+
         driver = new FirefoxDriver();
-        driver.navigate().to(SEARCH);
+        driver.navigate().to(getNextprotPageUrl(pageName));
     }
 
-    @Then("^I check page title is neXtProt platform$")
-    public void iCheckPageTitleIsNeXtProtPlatform() throws Throwable {
-        Assert.assertTrue(driver.getTitle().contains("neXtProt platform"));
+    @Then("^I check page title is \"([^\"]*)\"$")
+    public void pageTitleShouldBe(String expectedTitle) throws Throwable {
+        Assert.assertTrue(driver.getTitle().contains(expectedTitle));
     }
+
+    @When("^I navigate to \"([^\"]*)\"$")
+    public void shouldClickOnLink(String link) throws Throwable {
+
+        driver.findElement(By.linkText(link)).click();
+    }
+
+    @Then("^I should find in the page \"([^\"]*)\"$")
+    public void iShouldFindInThePage(String expectedName) throws Throwable {
+
+        Assert.assertTrue(driver.getPageSource().contains(expectedName));
+
+    }
+
+    @And("^I close the browser$")
+    public void iCloseTheBrowser() throws Throwable {
+        driver.close();
+    }
+
+    /* @And("^I am connected with my google account$")
+    public void iAmConnectedWithMyGoogleAccount() {
+    }
+
+    @Then("^I am logged \"([^\"]*)\"$")
+    public void iAmLogged(String logged) {
+    }
+
+    @Then("^I should see the Application release version$")
+    public void i_check_that_Application_release_is_defined() {
+
+        String text = driver.findElement(By.xpath("//p[@class=\"ng-binding\"]")).getText();
+        Assert.assertEquals("Data release: 2016-08-25", text);
+        driver.quit();
+    } */
 }
--- src/test/resources/features/access-nextprot.feature
+++ src/test/resources/features/access-nextprot.feature
@@ -4,10 +4,12 @@ Feature: neXtProt web application is accessible
   As a user I should be able to access neXtProt web site
 
   Scenario: Check nextprot search title
-    When I am on nextprot search page
-    Then I check page title is neXtProt platform
+    When I am on nextprot page "search"
+    Then I check page title is "neXtProt platform"
+    And I close the browser
 
   Scenario: Check release data version from nextprot search
-    Given I am on nextprot search page
-    When I click 'Release contents' link
-    Then I should see the Application release version
+    Given I am on nextprot page "search"
+    When I navigate to "Release contents"
+    Then I should find in the page "Application release"
+    And I close the browser
