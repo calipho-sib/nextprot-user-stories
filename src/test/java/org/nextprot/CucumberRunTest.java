ced6b6900730f5b4ab06821dcbfa9e00a5177862
fnikitin <frederic.nikitin@gmail.com>
2016-10-06 18:29:08 +0200

    Removing tag in CucumberRunTest

--- src/test/java/org/nextprot/CucumberRunTest.java
+++ src/test/java/org/nextprot/CucumberRunTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".", tags = "@active", plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(features = ".", plugin = {"pretty", "html:target/cucumber"})
 public class CucumberRunTest {
 
 }
